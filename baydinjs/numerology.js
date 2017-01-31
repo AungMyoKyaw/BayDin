003cc8f26aaafa8b8132cecddcc2643148806e07
Aung Myo Kyaw <amk.maeu@gmail.com>
2017-01-31 16:20:37 +0630

    baydinjs

---
+++ baydinjs/numerology.js
@@ -0,0 +1,17 @@
+function numberbaydin(num) {
+	//Aung Myo Kyaw
+    function numsplitreduce(a, b) {
+        return Number(a) + Number(b);
+    }
+    var numsplit = num.toString().split("");
+    var sum = numsplit.reduce(numsplitreduce);
+    //console.log(sum);
+    while (sum > 9) {
+    	numsplit=sum.toString().split("");
+    	sum = numsplit.reduce(numsplitreduce);
+    }
+    //console.log(sum);
+    return sum;
+}
+
+module.exports = numberbaydin;
---
+++ index.js
@@ -0,0 +1,13 @@
+var maharbote = require('./baydinjs/maharbote');
+var nayNum = require('./baydinjs/nayNum');
+var numerology = require('./baydinjs/numerology');
+var numFormat = require('./baydinjs/numFormat');
+var thatyout = require('./baydinjs/thatyout');
+
+module.exports = {
+	maharbote,
+	nayNum,
+	numerology,
+	numFormat,
+	thatyout
+}
